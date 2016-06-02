4dae423ce7fc9117b963fb3ba4ed8a364f8a12a8
Jose Cortes <jcortes5@eafit.edu.co>
2016-06-02 10:01:12 -0500

    fix bugs

--- js/matrizGenerator.js
+++ js/matrizGenerator.js
@@ -7,6 +7,8 @@ var matriz = {
 
     generate: function () {
         $("#matrizEcuaciones").html("");
+        $("#variablesId").html("");
+        matriz.idVariable = 0;
         var variables = $("#numeroVariables").val();
 
         var td = '<td><input class="inputMatriz" type="number"></input></td>'
@@ -15,23 +17,20 @@ var matriz = {
             td = td + '<td><input class="inputMatriz" type="number"></input></td>';
             th = th + '<td>X' + (matriz.idVariable + 1) + '</td>'
             matriz.idVariable++;
-           /* if (i = variables) {
-                th = th + '<td> B </td>';
-            }*/
+            /* if (i = variables) {
+                 th = th + '<td> B </td>';
+             }*/
         }
         if (variables > 0) {
             $("#variablesId").append(th);
             // td * (variables + 1) 
 
             for (var i = 0; i < variables; i++) {
-
-
-            }
-
-            for (var i = 0; i < variables; i++) {
                 $("#matrizEcuaciones").append('<tr>' + td + '</tr>')
 
             }
+
+        } else {
 
         }
 
