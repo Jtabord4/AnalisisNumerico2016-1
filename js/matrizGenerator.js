026c484fa63aef6f64515aed5f47c445e02a10b5
Jose  Cortes Jaramillo <josecortesjaramillo@MacBook-Pro-de-Jose.local>
2016-06-01 23:01:04 -0500

    Add type input

--- js/matrizGenerator.js
+++ js/matrizGenerator.js
@@ -7,9 +7,9 @@ var matriz = {
     generate: function () {
         $("#matrizEcuaciones").html("");
         var variables = $("#numeroVariables").val();
-        var td = '<td><input></input></td>'
+        var td = '<td><input class="inputMatriz" type="number"></input></td>'
         for (var i = 0; i < variables; i++) {
-            td = td + '<td><input type="number"></input></td>';
+            td = td + '<td><input class="inputMatriz" type="number"></input></td>';
         }
         if (variables > 0) {
             // td * (variables + 1) 
@@ -19,17 +19,11 @@ var matriz = {
 
         }
 
-
-
-
     },
 
 
     getValue: function () {
-        $("#iconAlert").click(function () {
-            $("#alertModal").modal('hide');
-            $(".panel-danger").show(500);
-        });
+
     }
 
 
