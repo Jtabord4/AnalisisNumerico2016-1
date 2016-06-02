38eb0cd51b1583747d4f4a02cdf5e2032f9321c8
Jose Cortes <jcortes5@eafit.edu.co>
2016-06-02 09:37:04 -0500

    Fix matriz bug

--- js/matrizGenerator.js
+++ js/matrizGenerator.js
@@ -3,18 +3,34 @@
 
 
 var matriz = {
+    idVariable: 0,
 
     generate: function () {
         $("#matrizEcuaciones").html("");
         var variables = $("#numeroVariables").val();
+
         var td = '<td><input class="inputMatriz" type="number"></input></td>'
+        var th = '<td>X' + matriz.idVariable + '</td>'
         for (var i = 0; i < variables; i++) {
             td = td + '<td><input class="inputMatriz" type="number"></input></td>';
+            th = th + '<td>X' + (matriz.idVariable + 1) + '</td>'
+            matriz.idVariable++;
+           /* if (i = variables) {
+                th = th + '<td> B </td>';
+            }*/
         }
         if (variables > 0) {
+            $("#variablesId").append(th);
             // td * (variables + 1) 
+
+            for (var i = 0; i < variables; i++) {
+
+
+            }
+
             for (var i = 0; i < variables; i++) {
                 $("#matrizEcuaciones").append('<tr>' + td + '</tr>')
+
             }
 
         }
