98c66a4edfd90a0c8e3025c7c586895b86512fed
Jose  Cortes Jaramillo <josecortesjaramillo@gmail.com>
2016-06-04 12:11:40 -0500

    first chapter done

--- js/captureData.js
+++ js/captureData.js
@@ -9,7 +9,7 @@ var capture = {
         var dataIncremental = [];
 
 
-          // obetener valores
+        // obetener valores
         $('.searchIn').each(function () {
             var data = ($(this).val());
             dataIncremental.push(data)
@@ -26,32 +26,103 @@ var capture = {
         console.log(searchOut)
 
         //envio de datos
-        /*     $.ajax({
-                 url: '...',
-                 type: 'post',
-                 data: JSON.stringify(searchOut),
-                 contentType: 'application/json',
-                 dataType: 'json'
-             });*/
-
-
-        // recibir datos
-        
-        
-        
-        
         $.ajax({
             url: '...',
-            type: 'GET',
-            contentType: false,
-            processData: false,
+            type: 'POST',
+            data: JSON.stringify(searchOut),
+            contentType: 'application/json',
+            dataType: 'json',
             success: function (response) {
+                //mostrar resultado
                 show.busquedaIncremental(JSON.parse(response))
             }
         });
 
-
     },
+
+    bisection: function () {
+        var bisectionData = [];
+        $('.bisection').each(function () {
+            var data = ($(this).val());
+            bisectionData.push(data)
+        });
+
+        var bisectionOut = {
+            'eq': dataIncremental[0],
+            'inicio': dataIncremental[1],
+            'iteraciones': dataIncremental[3],
+            'delta': dataIncremental[2],
+
+        }
+
+        $.ajax({
+            url: '...',
+            type: 'POST',
+            data: JSON.stringify(bisectionOut),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response) {
+                //mostrar resultado
+                show.bisection(JSON.parse(response))
+            }
+        });
+    },
+
+    newton: function () {
+        var newtonData = [];
+        $('.newton').each(function () {
+            var data = ($(this).val());
+            newtonData.push(data)
+        });
+
+        var newtonOutput = {
+            'eq': dataIncremental[0],
+            'inicio': dataIncremental[1],
+            'iteraciones': dataIncremental[3],
+            'delta': dataIncremental[2],
+
+        }
+
+        $.ajax({
+            url: '...',
+            type: 'POST',
+            data: JSON.stringify(newtonOutput),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response) {
+                //mostrar resultado
+                show.newton(JSON.parse(response))
+            }
+        });
+    },
+    fijo: function () {
+        var fijoData = [];
+        $('.fijo').each(function () {
+            var data = ($(this).val());
+            fijoData.push(data)
+        });
+
+        var fijoOutput = {
+            'eq': dataIncremental[0],
+            'inicio': dataIncremental[1],
+            'iteraciones': dataIncremental[3],
+            'delta': dataIncremental[2],
+
+        }
+
+        $.ajax({
+            url: '...',
+            type: 'POST',
+            data: JSON.stringify(fijoOutput),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response) {
+                //mostrar resultado
+                show.fijo(JSON.parse(response))
+            }
+        });
+
+    }
 
 
 }
--- js/showData.js
+++ js/showData.js
@@ -2,9 +2,28 @@
 var show = {
 
     busquedaIncremental: function (data) {
-            if(data.status === 'true'){
+        if (data.status === 'true') {
             var output = data.output;
             $('#resultadoSearch').html(output)
-            }
+        }
+    },
+
+    bisection: function (data) {
+        if (data.status === 'true') {
+            var output = data.output;
+            $('#resultadoBisection').html(output)
+        }
+    },
+    newton: function (data) {
+        if (data.status === 'true') {
+            var output = data.output;
+            $('#resultadoBisection').html(output)
+        }
+    },
+    fijo: function (data) {
+        if (data.status === 'true') {
+            var output = data.output;
+            $('#resultadoFijos').html(output)
+        }
     }
 }
