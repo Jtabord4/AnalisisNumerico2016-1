f92c799e65b621bfcae5d565f183d405dde492e5
Jose  Cortes Jaramillo <josecortesjaramillo@gmail.com>
2016-06-05 11:39:05 -0500

    show output incremental search

--- js/showData.js
+++ js/showData.js
@@ -3,8 +3,20 @@ var show = {
 
     busquedaIncremental: function (data) {
         if (data.status === 'true') {
-            var output = data.output;
-            $('#resultadoSearch').html(output)
+            for (var i = 0; i < data.output.length; i++) {
+                var x0 = data.output[i].x0;
+                var x1 = data.output[i].x1;
+                var f0 = data.output[i].fx0;
+                var f1 = data.output[i].fx1;
+                var out = '<li class="list-group-item">' +
+                    $('#resultadoSearch').append(
+                        '<li class="list-group-item">Intervalo :' + x0
+                        + '-' + x1
+                        + 'evaluando en los puntos'
+                        + f0 + 'y' 
+                        + f1 + '</li>'
+                    )
+            }
         }
     },
 
