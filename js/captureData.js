a3ff0ebcd6bcbedc44313fccff9dd868d2a212ec
Jtabord4 <Jpablox92@gmail.com>
2016-06-04 22:19:16 -0500

    captura data, placeholders, info integrales

--- js/captureData.js
+++ js/captureData.js
@@ -229,6 +229,42 @@ var capture = {
         });
 
 
+    },
+    
+    integracion: function () {
+        //arreglo de datos
+        var dataIntegracion = [];
+
+
+        // obetener valores
+        $('.integracionIn').each(function () {
+            var data = ($(this).val());
+            dataIntegracion.push(data)
+
+        });
+        //objeto json
+        var searchOut = {
+            '"eq"': dataIntegracion[0],
+            '"inicial"': dataIntegracion[1],
+            '"final"': dataIntegracion[2],
+            '"iteraciones"': dataIntegracion[3],
+
+        }
+        console.log(searchOut)
+
+        //envio de datos
+        $.ajax({
+            url: '...',
+            type: 'POST',
+            data: JSON.stringify(searchOut),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response) {
+                //mostrar resultado
+                show.integracion(JSON.parse(response))
+            }
+        });
+
     }
 
 
