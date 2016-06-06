41f4966b70255ac696517c32748b5e996d7761f3
Jose  Cortes Jaramillo <josecortesjaramillo@gmail.com>
2016-06-06 11:17:32 -0500

    parse

--- js/showData.js
+++ js/showData.js
@@ -2,6 +2,7 @@
 var show = {
 
     busquedaIncremental: function (data) {
+        JSON.parse(data);
         var x0 = data.output[i].x0;
         var x1 = data.output[i].x1;
         var f0 = data.output[i].fx0;
